44a0a83782739b756f1f10bbf2e8a4d81838e357
Santiagobelogi <santiagotbelogi@gmail.com>
2017-06-15 18:01:32 -0300

    Agrego menu bar en la pantalla de inicio
    tambien cambie tamaño de la ventana

--- tpSuperior.m
+++ tpSuperior.m
@@ -1,26 +1,33 @@
+
 pkg load control;
 pkg load signal;
 
 function init()
-  Inicio = figure;
-  set (Inicio,"name","Trabajo Practico - Matematica Superior");
-  set (Inicio,"color","white");
-  set (Inicio,"menubar","none");
+  Inicio = figure('Position',[400 400 600 170],'Units','Pixels');
+  set (Inicio,"name","T.P. Matematica Superior");
   set (Inicio,"numbertitle","off");
+  set (Inicio,"menubar","none");
 
   pantalla = uibuttongroup (Inicio, "position", [ 0 0 1 1],...
       "title","Menu","titleposition","centertop");
-  
+
+f = uimenu ("label", "&Archivo", "accelerator", "f");
+e = uimenu ("label", "&Acerca del TP", "accelerator", "f");
+uimenu (f, "label", "Cerrar", "accelerator", "q", ...
+           "callback", "close (gcf)");
+uimenu (e, "label", "Integrantes", "accelerator", "i", ...
+           "callback",{@mostrarIntegrantes});
+           
   tituloPrincipal = uicontrol (Inicio,"style","text", ...
-      "string","Seleccione datos a ingresar:","position",[85,300,400,50], ... 
+      "string","Seleccione datos a ingresar","position",[100,100,400,50], ... 
       "fontsize",20); 
   
   calculoConCoeficientes = uicontrol (pantalla,"string","Coeficientes", ...
-      "position",[50,150,200,100],"callback",{@ventanaCoeficientes}, ...
+      "position",[0,40,300,60],"callback",{@ventanaCoeficientes}, ...
       "backgroundcolor",[.8,.8,.8],"fontsize",13);
    
   calculoConDatosCPG = uicontrol (pantalla,"string","Ceros, polos y ganancia", ...
-      "position",[300,150,200,100],"callback",{@ventanaDatos}, ...
+      "position",[300,40,300,60],"callback",{@ventanaDatos}, ...
       "backgroundcolor",[.8, .8, .8  ],"fontsize",13);
 endfunction
 
