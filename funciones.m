8214383e465b970bfa301ba4d9076bf29060f59f
Nacho Blanco Bon <Nacho Blanco Bon>
2017-06-15 17:13:06 -0300

    Cambios funciones.m

--- funciones.m
+++ funciones.m
@@ -40,49 +40,56 @@ endfunction
 
 function calcularPolos (handlesource,event,coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia)
   funcionTransferencia = procesarFuncionTransferencia (coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia);
-  [z,p,g] = tf2zp(funcionTransferencia);
-  stringPolos= strcat("Los polos son ",evalc ("p"));
+  [ceros,polos,ganancia] = tf2zp(funcionTransferencia);
+  stringPolos= strcat("Los polos son ",evalc ("polos"));
   h = msgbox (stringPolos);
 endfunction
 
 function calcularCeros (handlesource,event,coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia)
   funcionTransferencia = procesarFuncionTransferencia (coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia);
-  [z,p,g] = tf2zp(funcionTransferencia);
-  stringCeros = strcat("Los ceros son ",evalc ("z"));
+  [ceros,polos,ganancia] = tf2zp(funcionTransferencia);
+  stringCeros = strcat("Los ceros son ",evalc ("ceros"));
   h = msgbox (stringCeros);
 endfunction
 
 function mostrarGanancia (handlesource,event,coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia)
   funcionTransferencia = procesarFuncionTransferencia (coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia);
-  [z,p,g] = tf2zp(funcionTransferencia);
-  stringGanancia = strcat("La ganancia del sistema es ", evalc ("g"));
+  [ceros,polos,ganancia] = tf2zp(funcionTransferencia);
+  stringGanancia = strcat("La ganancia del sistema es ", evalc ("ganancia"));
   h = msgbox (stringGanancia);
 endfunction
 
-function str = obtenerExpresionCPG (funcionTransferencia)
-  [z,p,g] = tf2zp(funcionTransferencia);
-  str = num2str (g);
-  cantidadCeros = length (z);
-  cantidadPolos = length (p);
+function str = expresionCerosPolosGanancia (funcionTransferencia)
+  [ceros,polos,ganancia] = tf2zp(funcionTransferencia);
+  
+  cantidadDeCeros = length (ceros);
+  cantidadDePolos = length (polos);
+  
+  str = num2str (ganancia);
+  #Para obtener la expresion CPG voy concatenando polos y ceros 
   for i = 1:cantidadCeros 
-    str = strcat(str, " (s - (",num2str (z(i)),")) ");
+    str = strcat(str, " (s - (",num2str (ceros(i)),")) ");
   endfor   
-  str = strcat(str, "\n");
+  str = strcat(str, "\n"); #Espacio
   
   str2 = "";
+  
   for i = 1:cantidadPolos 
-    str2 = strcat(str2, "(s - (",num2str (p(i)),")) ");
+    str2 = strcat(str2, "(s - (",num2str (polos(i)),")) ");
   endfor
+  
   strlen = length (str2);
   for i = 1:strlen
     str = strcat(str, "-");
   endfor
+  
   str = strcat(str, "\n");
   str = strcat(str, str2);
 endfunction  
+#CPG : ceros, polos, ganancia
 function calcularExpresionCPG (handlesource,event,coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia)
   funcionTransferencia = procesarFuncionTransferencia (coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia);
-  stringExpresion = strcat("La expresion de ceros, polos y ganancia es \n",obtenerExpresionCPG(funcionTransferencia));
+  stringExpresion = strcat("La expresion de ceros, polos y ganancia es \n",expresionCerosPolosGanancia(funcionTransferencia));
   h = msgbox (stringExpresion);
 endfunction
 
@@ -110,25 +117,25 @@ endfunction
 
 function calcularEstabilidad (handlesource,event,coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia)
   funcionTransferencia = procesarFuncionTransferencia (coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia);
-  [z,p,g] = tf2zp(funcionTransferencia);
+  [ceros,polos,ganancia] = tf2zp(funcionTransferencia);
   mensaje = esEstable(p);
   h = msgbox(mensaje,"Estabilidad");
 endfunction
 
 function calcularTodo (handlesource,event,coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia)
   funcionTransferencia = procesarFuncionTransferencia (coeficientesNumerador,coeficientesDenominador,coeficienteGanancia,hayGanancia);
-  [z,p,g] = tf2zp(funcionTransferencia);
+  [ceros,polos,ganancia] = tf2zp(funcionTransferencia);
   
   stringExpresion = strcat("La expresion de la funcion de transferencia es ",evalc ("funcionTransferencia"));
   h = msgbox (stringExpresion); 
   
-  stringCeros = strcat("Los ceros son ",evalc ("z"));
+  stringCeros = strcat("Los ceros son ",evalc ("ceros"));
   h = msgbox (stringCeros);
   
-  stringPolos= strcat("Los polos son ",evalc ("p"));
+  stringPolos= strcat("Los polos son ",evalc ("polos"));
   h = msgbox (stringPolos);
   
-  stringGanancia = strcat("La ganancia del sistema es ", evalc ("g"));
+  stringGanancia = strcat("La ganancia del sistema es ", evalc ("ganancia"));
   h = msgbox (stringGanancia);
   
   stringExpresion = strcat("La expresion de ceros, polos y ganancia es \n",obtenerExpresionCPG(funcionTransferencia));
