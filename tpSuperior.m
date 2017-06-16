68787e0ed49fbe7b790ce61bb8074449d6963a36
SiSoyYoFacu <dominguezfacundonicolas96@gmail.com>
2017-06-16 13:40:36 -0300

    La barra de menu de nuevo que la habia ocultado

--- tpSuperior.m
+++ tpSuperior.m
@@ -6,7 +6,6 @@ function init()
   Inicio = figure('Position',[400 400 600 170],'Units','Pixels');
   set (Inicio,"name","ASIC");
   set (Inicio,"numbertitle","off");
-  set (Inicio,"menubar","none");
 
   pantalla = uibuttongroup (Inicio, "position", [ 0 0 1 1],...
       "title","Menu","titleposition","centertop");
